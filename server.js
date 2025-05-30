e3c158105bc969c3b93e4996ef2063f29a3321dc
Avinash Andhale <avinashandhale999@gmail.com>
2025-05-30 14:04:47 +0530

    changes in bundle json file

--- server.js
+++ server.js
@@ -12,30 +12,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/plans', (req, res) => {
-  const usage = req.query.usage_type;
-  const name = req.query.name;
-
-  let usageFiltered = [];
-  let nameFiltered = [];
-
-  if (usage) {
-    usageFiltered = plans.filter(plan => plan.usage === usage);
-  }
-
-  if (usageFiltered.length > 0) {
-    return res.json(usageFiltered);
-  }
-
-  if (name) {
-    nameFiltered = plans.filter(plan =>
-      plan.name.toLowerCase().includes(name.toLowerCase())
-    );
-  }
-
-  if (nameFiltered.length > 0) {
-    return res.json(nameFiltered);
-  }
-  res.json([]);
+  res.json(plans);
 });
 
 app.get('/bundled', (req, res) => {
